b09ec39ec179252ac180644a5713a3f92957a905
m-a-hossain <adam.eve007@gmail.com>
2021-02-03 22:25:21 +0600

    creating guadians and uniperson class

---
+++ person/Guardian.js
@@ -0,0 +1,36 @@
+const Person = require('./Person')
+
+const _profession = Symbol('profession')
+const _income = Symbol('income')
+
+class Guardian extends Person{
+    constructor(id, name){
+       super(id,name);
+    this[_income] =null
+    this[_profession] = null
+    }
+     
+    get income(){
+        return this[_income]
+    }
+
+    set income(value){
+        this[_income] =value
+    }
+
+    get profession(){
+        return this[_profession]
+    }
+
+    set profession(value){
+        this[_profession] = value 
+    }
+
+    toString(){
+        super.toString();
+        console.log('Accounts= ${this[_account]}')
+        console.log('Professin= ${this[_profession]}')
+    }
+}
+
+export default Guardian;
---
+++ person/Uniperson.js
@@ -0,0 +1,36 @@
+const Person= require('./Person')
+
+const _department =Symbol('department')
+const _account =Symbol('account')
+
+class Uniperson extends Person{
+    constructor(id,name){
+        super(id,name)
+      this[_department] =null;
+      this[_account] =null;
+    }
+  
+    get department(){
+        return this[_department]
+    }
+
+    set department(value){
+        this[_department]=value
+    }
+
+    get account(){
+        this[_account]= account
+    }
+
+    set account(value){
+        this[_account]=value
+    }
+
+    toString(){
+      super.toString()
+      console.log('Deartment= ${this[_department]}')
+      console.log('Account= ${this[_account]}')
+    }
+}
+
+export default Uniperson;
